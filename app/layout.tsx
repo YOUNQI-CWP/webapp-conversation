5ff30dc9cb7a142087c7250f9b833d8f56bb6521
YOUNGQI <116346775+YOUNQI-CWP@users.noreply.github.com>
2025-02-22 22:25:45 +0800

    Update layout.tsx
    修改标题配置

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { getLocaleOnServer } from '@/i18n/server'
+import Head from 'next/head' // 确保正确导入 Head 组件
 
 import './styles/globals.css'
 import './styles/markdown.scss'
@@ -13,6 +14,9 @@ const LocaleLayout = ({
     <html lang={'zh-Hans'} className="h-full">
       <body className="h-full">
         <div className="overflow-x-auto">
+          <Head>
+            <title>您的网页标题</title>
+          </Head>
           <div className="w-screen h-screen min-w-[300px]">
             {children}
           </div>
